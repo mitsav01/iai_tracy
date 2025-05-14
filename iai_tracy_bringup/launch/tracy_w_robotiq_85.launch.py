667f5d0fc3345b4af010e41d44c35a65d4674d8e
mitsav01 <miteshsavsaviya@gmail.com>
2025-05-14 04:42:26 +0200

    organised namespace for left and right arm

--- iai_tracy_bringup/launch/tracy_w_robotiq_85.launch.py
+++ iai_tracy_bringup/launch/tracy_w_robotiq_85.launch.py
@@ -1,105 +1,241 @@
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.substitutions import (
     Command,
     FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
 )
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 def generate_launch_description():
 
+    #Initialize config files
 
-    ur_type = LaunchConfiguration("ur_type")
-    robot_ip = LaunchConfiguration("robot_ip")
-    # General arguments
-    kinematics_params_file = LaunchConfiguration("kinematics_params_file")
-    joint_limit_params_file = LaunchConfiguration("joint_limit_params_file")
-    description_file = LaunchConfiguration("description_file")
-    tf_prefix = LaunchConfiguration("tf_prefix")
-    reverse_ip = LaunchConfiguration("reverse_ip")
-    script_command_port = LaunchConfiguration("script_command_port")
-    reverse_port = LaunchConfiguration("reverse_port")
-    script_sender_port = LaunchConfiguration("script_sender_port")
-    trajectory_port = LaunchConfiguration("trajectory_port")
-
-    # Left arm
-    left_arm_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                FindPackageShare('ur_robot_driver').find('ur_robot_driver'),
-                'launch',
-                'ur10e_bringup.launch.py'
-            )
-        ),
-        launch_arguments={
-            'robot_ip': '192.168.102.154',
-            'tf_prefix': 'left_',
-            'controller_config_file': os.path.join(
-                FindPackageShare('iai_tracy_ur').find('iai_tracy_ur'),
-                'config',
-                'ur10e_controllers_left.yaml'
-            ),
-            'controllers': 'joint_state_controller_left scaled_pos_joint_traj_controller_left',
-            'stopped_controllers': 'pos_joint_traj_controller_left',
-            'kinematics_config': LaunchConfiguration('kinematics_config_left'),
-            'robot_description_file': os.path.join(
-                FindPackageShare('iai_tracy_description').find('iai_tracy_description'),
-                'launch',
-                'upload.launch.py'
-            ),
-            'reverse_port': '50011',
-            'script_sender_port': '50012',
-            'trajectory_port': '50013',
-            'script_command_port': '50014'
-        }.items()
+    left_kinematics_params = PathJoinSubstitution(
+        [FindPackageShare("iai_tracy_ur"),"include","iai_tracy_ur","left_ur10e_calibration.yaml"]
+    )
+    left_controller_fl = PathJoinSubstitution(
+        [FindPackageShare("iai_tracy_ur"),"config","ur10e_controllers_left.yaml"]
+    )
+    right_kinematics_params = PathJoinSubstitution(
+        [FindPackageShare("iai_tracy_ur"),"include","iai_tracy_ur","right_ur10e_calibration.yaml"]
+    )
+    right_controller_fl = PathJoinSubstitution(
+        [FindPackageShare("iai_tracy_ur"),"config","ur10e_controllers_right.yaml"]
     )
 
-    # Right arm
-    right_arm_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                FindPackageShare('ur_robot_driver').find('ur_robot_driver'),
-                'launch',
-                'ur10e_bringup.launch.py'
-            )
-        ),
-        launch_arguments={
-            'robot_ip': '192.168.102.153',
-            'tf_prefix': 'right_',
-            'controller_config_file': os.path.join(
-                FindPackageShare('iai_tracy_ur').find('iai_tracy_ur'),
-                'config',
-                'ur10e_controllers_right.yaml'
-            ),
-            'controllers': 'joint_state_controller_right scaled_pos_joint_traj_controller_right',
-            'stopped_controllers': 'pos_joint_traj_controller_right',
-            'kinematics_config': LaunchConfiguration('kinematics_config_right'),
-            'robot_description_file': os.path.join(
-                FindPackageShare('iai_tracy_description').find('iai_tracy_description'),
-                'launch',
-                'upload.launch.py'
-            ),
-            'reverse_port': '50001',
-            'script_sender_port': '50002',
-            'trajectory_port': '5003',
-            'script_command_port': '50005'
-        }.items()
+    description_launch_fl = PathJoinSubstitution(
+        [FindPackageShare("iai_tracy_description"),"launch","display.launch.py"]
     )
+
+    ur_type = LaunchConfiguration('ur_type') 
+
+    description_launch_file = LaunchConfiguration('description_launch_file')
+    left_robot_ip = LaunchConfiguration('left_robot_ip') 
+    left_controller_config_file = LaunchConfiguration('left_controller_config_file') 
+    left_tf_prefix = LaunchConfiguration('left_tf_prefix') 
+    left_script_command_port = LaunchConfiguration('left_script_command_port')
+    left_trajectory_port = LaunchConfiguration('left_trajectory_port')
+    left_reverse_port = LaunchConfiguration('left_reverse_port')
+    left_script_sender_port = LaunchConfiguration('left_script_sender_port')
+    left_controllers = LaunchConfiguration('left_controllers')
+    left_stopped_controllers = LaunchConfiguration('left_stopped_controller')
+    left_kinematic_config = LaunchConfiguration('left_kinematic_config')
+    
+
+    right_robot_ip = LaunchConfiguration('right_robot_ip') 
+    right_controller_config_file = LaunchConfiguration('right_controller_config_file') 
+    right_tf_prefix = LaunchConfiguration('right_tf_prefix') 
+    right_script_command_port = LaunchConfiguration('right_script_command_port')
+    right_trajectory_port = LaunchConfiguration('right_trajectory_port')
+    right_reverse_port = LaunchConfiguration('right_reverse_port')
+    right_script_sender_port = LaunchConfiguration('right_script_sender_port')
+    right_controllers = LaunchConfiguration('right_controllers')
+    right_stopped_controllers = LaunchConfiguration('right_stopped_controller')
+    right_kinematic_config = LaunchConfiguration('right_kinematic_config')
+
+
+    # # UR specific arguments
+    ur_type_arg = DeclareLaunchArgument(
+            "ur_type",
+            default_value='ur10e',
+            description="Type/series of used UR robot.",
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20"],
+    )
+    description_launch_file_arg = DeclareLaunchArgument(
+        "description_launch_file",
+        default_value=description_launch_fl,
+        description="publishes the description topic."
+    )
+    left_robot_ip_arg = DeclareLaunchArgument(
+        "left_robot_ip",
+        default_value='192.168.102.154',
+        description="IP address by which the robot can be reached.",
+    )
+    left_tf_prefix_arg = DeclareLaunchArgument(
+        "left_tf_prefix",
+        default_value="left",
+            description="tf_prefix of the joint names, useful for \
+            multi-robot setup. If changed, also joint names in the controllers' configuration \
+            have to be updated.",
+    )
+    left_controller_config_file_arg = DeclareLaunchArgument(
+        "left_controller_config_file",
+        default_value=left_controller_fl,
+        description="YAML file with the controllers configuration.",
+    )
+    left_controllers_arg = DeclareLaunchArgument(
+        "left_controllers",
+        default_value="joint_state_controller_left scaled_pos_joint_traj_controller_left",
+        description="controllers used in robot's operation",
+    )
+    left_stopped_controllers_arg = DeclareLaunchArgument(
+        "left_stopped_controllers",
+        default_value="pos_joint_traj_controller_left",
+    )
+    left_kinematic_config_arg = DeclareLaunchArgument(
+        "left_kinematic_config",
+        default_value=left_kinematics_params,
+    )
+    left_reverse_port_arg = DeclareLaunchArgument(
+        "left_reverse_port",
+        default_value="50011",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    left_script_sender_port_arg = DeclareLaunchArgument(
+        "left_script_sender_port",
+        default_value="50012",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    left_trajectory_port_arg = DeclareLaunchArgument(
+        "left_trajectory_port",
+        default_value="50013",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    left_script_command_port_arg = DeclareLaunchArgument(
+        "left_script_command_port",
+        default_value="50014",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+
+
+    right_robot_ip_arg = DeclareLaunchArgument(
+        "right_robot_ip",
+        default_value='192.168.102.154',
+        description="IP address by which the robot can be reached.",
+    )
+    right_tf_prefix_arg = DeclareLaunchArgument(
+        "right_tf_prefix",
+        default_value="right",
+            description="tf_prefix of the joint names, useful for \
+            multi-robot setup. If changed, also joint names in the controllers' configuration \
+            have to be updated.",
+    )
+    right_controller_config_file_arg = DeclareLaunchArgument(
+        "right_controller_config_file",
+        default_value=right_controller_fl,
+        description="YAML file with the controllers configuration.",
+    )
+    right_controllers_arg = DeclareLaunchArgument(
+        "right_controllers",
+        default_value="joint_state_controller_right scaled_pos_joint_traj_controller_right",
+        description="controllers used in robot's operation",
+    )
+    right_stopped_controllers_arg = DeclareLaunchArgument(
+        "right_stopped_controllers",
+        default_value="pos_joint_traj_controller_right",
+    )
+    right_kinematic_config_arg = DeclareLaunchArgument(
+        "right_linematic_config",
+        default_value=right_kinematics_params,
+    )
+    right_reverse_port_arg = DeclareLaunchArgument(
+        "right_reverse_port",
+        default_value="50011",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    right_script_sender_port_arg = DeclareLaunchArgument(
+        "right_script_sender_port",
+        default_value="50012",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    right_trajectory_port_arg = DeclareLaunchArgument(
+        "right_trajectory_port",
+        default_value="50013",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+    right_script_command_port_arg = DeclareLaunchArgument(
+        "right_script_command_port",
+        default_value="50014",
+        description="Port that will be opened to forward script commands from the driver to the robot",
+    )
+
+    ur_launch_dir = get_package_share_directory('ur_robot_driver')
+
+    left = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(ur_launch_dir, 'launch', 'ur_control.launch.py')),
+        launch_arguments={'ur_type': ur_type,
+                          'robot_ip': left_robot_ip,
+                          'controllers_file': left_controller_config_file,
+                          'tf_prefix': left_tf_prefix,
+                          'script_command_port': left_script_command_port,
+                          'trajectory_port': left_trajectory_port,
+                          'reverse_port': left_reverse_port,
+                          'script_sender_port': left_script_sender_port,
+                          }.items())
+    
+    left_with_namespace = GroupAction(
+     actions=[
+         PushRosNamespace('left'),
+         left
+      ]
+    )
+
+    right = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(ur_launch_dir, 'launch', 'ur_control.launch.py')),
+        launch_arguments={'ur_type': ur_type,
+                          'robot_ip': right_robot_ip,
+                          'controllers_file': right_controller_config_file,
+                          'tf_prefix': right_tf_prefix,
+                          'script_command_port': right_script_command_port,
+                          'trajectory_port': right_trajectory_port,
+                          'reverse_port': right_reverse_port,
+                          'script_sender_port': right_script_sender_port,
+                          }.items())
+    
+    right_with_namespace = GroupAction(
+     actions=[
+         PushRosNamespace('right'),
+         right
+      ]
+    )
+
+
+
 
     return LaunchDescription([
-        DeclareLaunchArgument('kinematics_config_left'),
-        DeclareLaunchArgument('kinematics_config_right'),
-        left_arm_launch,
-        right_arm_launch
+        ur_type_arg,
+        left_robot_ip_arg,
+        left_controller_config_file_arg,
+        left_tf_prefix_arg,
+        left_script_command_port_arg,
+        left_trajectory_port_arg,
+        left_reverse_port_arg,
+        left_script_sender_port_arg,
+        right_robot_ip_arg,
+        right_controller_config_file_arg,
+        right_tf_prefix_arg,
+        right_script_command_port_arg,
+        right_trajectory_port_arg,
+        right_reverse_port_arg,
+        right_script_sender_port_arg,
+        left_with_namespace,
+        right_with_namespace,
+        # description_launch_file_arg
     ])
